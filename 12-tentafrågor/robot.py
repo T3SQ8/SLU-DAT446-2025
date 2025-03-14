a024b0152f89c1b6f821793f4a061e5630bbe58a
T3SQ8 <mustafaibrahim672@gmail.com>
2025-03-14 15:23:31 +0100

    Mindre anteckningar till facit pass 8

--- 12-tentafrågor/robot.py
+++ 12-tentafrågor/robot.py
@@ -3,22 +3,33 @@ class Robot:
     self.x, self.y = 0, 0
     self.directions = ('NORTH', 'EAST', 'SOUTH', 'WEST')
     self.direction = self.directions[0]
-    
+
   def forward(self, steps):
+      # NOTE optimalt sätt att göra detta, tyvärr inte
+      # NOTE implementerat i Self-Practice (lades till Python år 2021)
+      # match self.direction:
+      #   case 'NORTH':
+      #       self.y += steps
+      #   case 'EAST':
+      #       self.x += steps
+      #   case 'SOUTH':
+      #       self.y -= steps
+      #   case 'WEST':
+      #       self.x -= steps
     if self.direction == 'NORTH':
       self.y += steps
-    if self.direction == 'EAST':
+    elif self.direction == 'EAST':
       self.x += steps
-    if self.direction == 'SOUTH':
+    elif self.direction == 'SOUTH':
       self.y -= steps
-    if self.direction == 'WEST':
+    elif self.direction == 'WEST':
       self.x -= steps
-    
+
   def turnRight(self):
     index = self.directions.index(self.direction) # get index of current direction
     index = (index + 1) % len(self.directions) # get next direction, jump back if at the end
     self.direction = self.directions[index]
-  
+
   def turnLeft(self):
     index = self.directions.index(self.direction)
     index = (index - 1) % len(self.directions)
